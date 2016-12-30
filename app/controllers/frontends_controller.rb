be5e935093bed74257e85678071b0469a6778d15
htoomyataung <htoomyatag.webdev@gmail.com>
2016-12-30 10:19:50 +0630

    img responsive work

--- app/controllers/frontends_controller.rb
+++ app/controllers/frontends_controller.rb
@@ -415,30 +415,51 @@ def chat_to_seller
   def round_up(number)
 
 
-
-    last_three_number = (number.to_s).last(3).to_s
-    mylast_fourth_number = (number.to_s).last(4).to_s
-    fourth_number = (number.to_s).last(4).to_s.first
+  
+  #   last_three_number = (number.to_s).last(3).to_s
+  #   mylast_fourth_number = (number.to_s).last(4).to_s
+  #   fourth_number = (number.to_s).last(4).to_s.first
+  
+    
+  #   if 500 <= last_three_number.to_i
 
     
-    if 500 <= last_three_number.to_i
-
-    
-      last_fourth_number = fourth_number.to_i+1
-      fourth_number = last_fourth_number.to_s+"000"
-      $product_price = (number.to_s).gsub(mylast_fourth_number,fourth_number)
+  #     @last_fourth_number = fourth_number.to_i+1
+  #     @fourth_number = @last_fourth_number.to_s+"000"
+  #     $product_price = (number.to_s).gsub(mylast_fourth_number,@fourth_number)
 
    
 
-    elsif 500 > last_three_number.to_i
+  #   elsif 500 > last_three_number.to_i
 
-      last_fourth_number = fourth_number.to_i
-      fourth_number = last_fourth_number.to_s+"000"
-      $product_price = (number.to_s).gsub(mylast_fourth_number,fourth_number)
+  #     @last_fourth_number = fourth_number.to_i
+  #     @fourth_number = @last_fourth_number.to_s+"000"
+  #     $product_price = (number.to_s).gsub(mylast_fourth_number,@fourth_number)
 
 
-    else
-  end
+  #   else
+  # end
+
+
+length = (number.to_s).length
+my_length = length-3
+  first_three_number = (number.to_s).first(my_length).to_s
+  last_three_number = (number.to_s).last(3).to_s
+
+
+      if 500 <= last_three_number.to_i
+
+        myfirst_three_number = first_three_number.to_i+1
+        $product_price = myfirst_three_number.to_s+"000"
+
+      elsif 500 > last_three_number.to_i
+
+
+          myfirst_three_number = first_three_number.to_i
+          $product_price = myfirst_three_number.to_s+"000"
+
+      end
+
    
     
   end
