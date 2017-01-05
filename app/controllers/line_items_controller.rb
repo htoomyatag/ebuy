5e4d26e08ed42e5994c1566f3ac0eecaca50940b
htoomyataung <htoomyatag.webdev@gmail.com>
2017-01-05 11:10:59 +0630

    bug fixed_completed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,6 +23,35 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   # POST /line_items.json
+
+  def round_up(number)
+
+
+
+
+length = (number.to_s).length
+my_length = length-3
+  first_three_number = (number.to_s).first(my_length).to_s
+  last_three_number = (number.to_s).last(3).to_s
+
+
+      if 500 <= last_three_number.to_i
+
+        myfirst_three_number = first_three_number.to_i+1
+        $product_price = myfirst_three_number.to_s+"000"
+
+      elsif 500 > last_three_number.to_i
+
+
+          myfirst_three_number = first_three_number.to_i
+          $product_price = myfirst_three_number.to_s+"000"
+
+      end
+
+   
+    
+  end
+
   def create
       @cart = current_cart
       product = Product.find(params[:product_id])
