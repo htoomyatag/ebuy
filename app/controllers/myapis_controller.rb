02b334ea452f77b4610de2d469a7667a8c703ebb
htoomyataung <htoomyatag.webdev@gmail.com>
2017-02-21 15:19:45 +0630

    bug fixed

--- app/controllers/myapis_controller.rb
+++ app/controllers/myapis_controller.rb
@@ -59,7 +59,7 @@ protect_from_forgery with: :null_session
           c = '],"' 
           my_third_json = my_seconday_json.gsub(a , "[")
           my_fourth_json = my_third_json.gsub(b , "]")
-          my_fifth_json = my_fourth_json.gsub(b , ']","')
+          my_fifth_json = my_fourth_json.gsub(c , ']","')
           format.json {render json: my_primary_json}
           format.text {render text: my_fifth_json}
     end
