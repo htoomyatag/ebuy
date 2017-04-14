4d842d74cdca545bb03261c928f3d01ed82b5c70
htoomyataung <htoomyatag.webdev@gmail.com>
2017-04-14 19:09:23 +0630

    sign up work

--- app/controllers/myapi/v1/registrations_controller.rb
+++ app/controllers/myapi/v1/registrations_controller.rb
@@ -14,8 +14,8 @@ class Myapi::V1::RegistrationsController < Devise::RegistrationsController
       render :status => 200,
            :json => { :success => true,
                       :info => "Registered",
-                      :data => { :user => resource,
-                                 :auth_token => current_user.authentication_token, :user_id => current_user.id, :user_name => current_user.name, :email => current_user.email, :user_type_id => current_user.user_type_id} }
+                      :data => { :buyer => resource,
+                                 :auth_token => current_buyer.authentication_token, :buyer_id => current_buyer.id} }
     else
       render :status => :unprocessable_entity,
              :json => { :success => false,
@@ -25,49 +25,26 @@ class Myapi::V1::RegistrationsController < Devise::RegistrationsController
   end
 
 
-  def user_new
-    @user = User.create(
-      :user_type_id =>  params[:user_type] ,
-      :name => params[:name], 
+  def buyer_new
+    @buyer = Buyer.create(
+
+      :buyer_name => params[:buyer_name],
+      :buyer_phone => params[:buyer_phone],
+      :buyer_township => params[:buyer_township],
+      :buyer_address => params[:buyer_address],
       :email => params[:email],
       :password => params[:password],
-      :password_confirmation => params[:password_confirmation],
-      :phone => params[:phone],
-
-      :personal_name => params[:personal_name],
-      :personal_address => params[:personal_address],
-      :personal_nrc_number => params[:personal_nrc_number],
-      :personal_nrc_front => params[:personal_nrc_front],
-      :personal_nrc_back => params[:personal_nrc_back],
-
-      :staff_name => params[:staff_name],
-      :staff_nrc_number => params[:staff_nrc_number],
-      :staff_address => params[:staff_address],
-      :staff_nrc_front => params[:staff_nrc_front],
-      :staff_nrc_back => params[:staff_nrc_back],
-      :company_licensce => params[:company_licensce],
-      :company_name => params[:company_name],
-
-      :retail_name => params[:retail_name],
-      :retail_address => params[:retail_address],
-      :retail_nrc_number => params[:retail_nrc_number],
-      :retail_nrc_front => params[:retail_nrc_front],
-      :retail_nrc_back => params[:retail_nrc_back],
-      :shop_licensce => params[:shop_licensce],
-      :shop_name => params[:shop_name]
+      :password_confirmation => params[:password_confirmation]
 
       )
   end
 
 
 
-
-
-
 private
 
 def sign_up_params
-    params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
+    params.require(:buyer).permit(:buyer_name, :buyer_phone, :buyer_township, :buyer_address, :email, :password, :password_confirmation)
 end
 
  
